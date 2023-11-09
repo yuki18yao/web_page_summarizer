298b287b5679a0c4f12be79d73ecc687c8c8adc8
Yuki Yao <yy012@bucknell.edu>
2023-11-09 15:27:08 -0500

    improved prompt engineering

--- main.py
+++ main.py
@@ -24,10 +24,11 @@ app.add_middleware(
 @app.get("/")
 def read_root():
   USER_MESSAGE = input("Enter the URL: ")
-  SYSTEM_MESSAGE = "You are a helpful assistant that summarizes the content of a webpage. Summarize and elaborate on the web page by finding central arguments. Then, list out supporting evidences the article uses. Do not contain information that are not in the web page. Be as specific as possible. "
-
+  SYSTEM_MESSAGE = "You are a highly skilled AI trained in language comprehension and summarization. I would like you to read the following text and summarize it into a concise abstract paragraph. Aim to retain the most important points, providing a coherent and readable summary that could help a person understand the main points of the discussion without needing to read the entire text. Please avoid unnecessary details or tangential points."
   completion = openai.ChatCompletion.create(
       model="gpt-3.5-turbo",
+      #model="gpt-4",
+      temperature=0,
       messages=[
           {"role": "system", "content": SYSTEM_MESSAGE},
           {"role": "user", "content": USER_MESSAGE},
